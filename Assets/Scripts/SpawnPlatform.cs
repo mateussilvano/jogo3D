3d585478167d2f0d7ec82a7e465d7aaa3e372867
mateussilvano <mateus_silvano@hotmail.com>
2023-11-08 22:34:39 -0300

    PLatform
    Platform

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -6,19 +6,51 @@ public class SpawnPlatform : MonoBehaviour
 {
 
     public List<GameObject> platforms = new List<GameObject>();
+    public List<Transform> currentPlatforms = new List<Transform>();
+
     public int offset;
+
+    private Transform vehicle;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
+    
     void Start()
     {
+
+        vehicle = GameObject.FindGameObjectWithTag("Player").transform;
+
         for(int i = 0; i < platforms.Count; i++)
         {
-            Instantiate(platforms[i], new Vector3(0,0,0), transform.rotation);
+            Transform p = Instantiate(platforms[i], new Vector3(0, 0, i * 204), transform.rotation).transform;
+            currentPlatforms.Add(p);
+            offset += 204;
         }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distance = vehicle.position.z - currentPlatformPoint.position.z;
+
+        if (distance >= 5)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > currentPlatforms.Count -1) 
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+        }
+    }
+
+    public void Recycle(GameObject platform)
+    {
+        platform.transform.position = new Vector3(0, 0, offset);
+        offset += 204;
     }
 }
